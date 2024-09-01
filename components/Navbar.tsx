20f52e594d64b5ca260959301e46a7508b508915
Shahbaj Sheikh <thisisshahbaj@gmail.com>
2024-09-01 21:02:47 +0530

    added stream

---
+++ components/Navbar.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import MobileNav from "./MobileNav";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+
+const Navbar = () => {
+  return (
+    <nav className="flex-between fixed z-50 w-full bg-dark-1 px6 py4 lg:px-10">
+      <Link href="/" className="flex items-center gap-1 ">
+        <Image
+          src="/icons/logo.svg"
+          width={32}
+          height={32}
+          alt="Face Time logo"
+        />
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          Face Time
+        </p>
+      </Link>
+      <div className="flex-between gap-5">
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+
+        <MobileNav />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
